f310b2f766a082d4f6885e23acdbd30ee972c609
YuriyNik156 <yuriy.nik156@gmail.com>
2025-06-26 15:07:52 +0300

    create project structure

--- GeneralNews/GeneralNews/settings.py
+++ GeneralNews/GeneralNews/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'news'
+    'news',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
