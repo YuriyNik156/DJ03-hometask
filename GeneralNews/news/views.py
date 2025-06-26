27498b794d43edbca6e1670bde2a3b474c18fe7d
YuriyNik156 <yuriy.nik156@gmail.com>
2025-06-26 15:30:07 +0300

    Update views.py

--- GeneralNews/news/views.py
+++ GeneralNews/news/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import News_post
 
-def home(request):
+def news_home(request):
     news = News_post.objects.all()
     return render(request, 'news/news.html', {'news':news})
